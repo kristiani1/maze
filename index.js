c6b5ee37216b7d4822b789f3ce3a50cb73447196
Kristian Toivonen <krtoivon@edu.lapinamk.fi>
2020-12-12 16:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const {
     Render, 
     Runner, 
     World, 
-    Bodies, 
-    MouseConstraint, 
-    Mouse 
+    Bodies
 } = Matter;
 
-const width = 800;
+const cells = 3; 
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -17,6 +16,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
+        wireframes: true,
         width,
         height
     }
@@ -24,24 +24,26 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
-
 // walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true}),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true}),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true})
 ];
 
 World.add(world, walls);
 
-// random shapes
-for (let i = 0; i < 20; i++) {
-    World.add(
-        world, 
-        Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50),
-    );
-}
+// Maze generation
+
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array (cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
